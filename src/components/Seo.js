19615451f23e422d67b2c99af6f3a3b7b79ab9ba
Scott Moses Sunarto <github@smsunarto.com>
2021-06-18 07:54:15 +0700

    add missing import

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
+import { useLocation } from '@reach/router'
 import { useStaticQuery, graphql } from 'gatsby'
 import ogImage from '../images/og-image.png'
 
